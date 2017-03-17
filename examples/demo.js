1f72fff05e873909badc51abfe968c6839bc1624
Darren DeRidder <darren.deridder@cenx.com>
2017-03-17 14:36:47 -0400

    more testing output

--- examples/demo.js
+++ examples/demo.js
@@ -1,7 +1,9 @@
 var jsummary = require('../lib/index.js');
+var format = jsummary.format;
 var ndjson = require("ndjson");
 var summarize  = jsummary.summarize;
 var fs = require('fs');
+var GenerateSchema = require('generate-schema');
 
 exports.demo = function demo() {
     var data = fs.readFileSync(__dirname + '/data.json');
@@ -14,4 +16,11 @@ exports.demo = function demo() {
     }
     var stats = summarize({}, jsonData);
     fs.writeFileSync(__dirname + "/output.json", JSON.stringify(stats, null, 2));
+
+    var html = format(stats);
+    fs.writeFileSync(__dirname + "/output.html", html);
+
+    var schema = GenerateSchema.json(jsonData);
+    fs.writeFileSync(__dirname + "/output.schema.json", JSON.stringify(schema, null, 2));
+
 };
